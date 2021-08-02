cf5d76cd8241b6879e650da45a3b296ab224fb13
firdaus-hqq <firdaushaqiqi17@gmail.com>
2021-08-02 11:06:01 +0700

    Menambah absen, data absen, dan memperbaiki navbar beranda dan tugas

--- web-sekolah/adminku/v_data_absen.php
+++ web-sekolah/adminku/v_data_absen.php
@@ -1,11 +1,14 @@
 <?php
-session_start();
-error_reporting(0);
-include "../configurasi/koneksi.php";
-include "../configurasi/library.php";
-include "../configurasi/fungsi_indotgl.php";
-include "../configurasi/fungsi_combobox.php";
+include "../config/config.php";
+include "configurasi/koneksi.php";
+include "configurasi/library.php";
+include "configurasi/fungsi_indotgl.php";
+include "configurasi/fungsi_combobox.php";
 include "timeout.php";
+
+$sql = 'SELECT * FROM data_absen INNER JOIN siswa ON (data_absen.id_siswa = siswa.id_siswa)';
+$listDataAbsen = $mysqli->query($sql);
+
 if ($_SESSION['login'] == 1) {
     if (!cek_login()) {
         $_SESSION['login'] = 0;
@@ -28,7 +31,6 @@ if ($_SESSION['login'] == 0) {
         echo "<input type=button class='btn btn-primary' value='LOGI DI SINI' onclick=location.href='../login_siswa.php'></a></center>";
     } else {
 ?>
-
         <!DOCTYPE html>
         <html>
         <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
@@ -37,7 +39,7 @@ if ($_SESSION['login'] == 0) {
         <meta name="author" content="ThemeBucket">
         <title>Halaman Siswa</title>
         <!-- Bootstrap 3.3.4 -->
-        <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <!-- Font Awesome Icons -->
         <link href="plugins/font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <link href="plugins/ionicons/css/ionicons.min.css" rel="stylesheet" type="text/css" />
@@ -67,6 +69,23 @@ if ($_SESSION['login'] == 0) {
             <link rel="stylesheet" href="plugins/file-uploader/css/jquery.fileupload-ui-noscript.css">
         </noscript>
         <link href="plugins/datepicker/datepicker.css" rel="stylesheet">
+
+        <script type="text/javascript">
+            window.setTimeout("waktu()", 1000);
+
+            function waktu() {
+                var tanggal = new Date();
+                setTimeout("waktu()", 1000);
+                document.getElementById("output").innerHTML = tanggal.getHours() + ":" + tanggal.getMinutes() + " WIB";
+            }
+            window.setTimeout("waktu_m()", 1000);
+
+            function waktu_m() {
+                var tanggal = new Date();
+                setTimeout("waktu_m()", 1000);
+                document.getElementById("output_m").innerHTML = tanggal.getHours() + ":" + tanggal.getMinutes() + " WIB";
+            }
+        </script>
         </head>
 
         <body class="skin-blue sidebar-mini">
@@ -78,9 +97,9 @@ if ($_SESSION['login'] == 0) {
                     <!-- Logo -->
                     <a href="index2.html" class="logo">
                         <!-- mini logo for sidebar mini 50x50 pixels -->
-                        <span class="logo-mini"><b>R</b>PL</span>
+                        <span class="logo-mini"><b>I</b>DM</span>
                         <!-- logo for regular state and mobile devices -->
-                        <span class="logo-lg"><b>Siswa</b>RPL</span>
+                        <span class="logo-lg"><b>IDM | </b> A C A D E M Y</span>
                     </a>
 
                     <!-- Header Navbar -->
@@ -116,12 +135,13 @@ if ($_SESSION['login'] == 0) {
                                         <li class="user-footer">
                                             <div class="pull-left">
                                                 <?php echo "
-                      <a href='media.php?module=siswa&act=detailprofilsiswa&id=$_SESSION[idsiswa]' class='btn btn-default btn-flat'>Profile</a>
-                    </div>
-                    <div class='pull-right'>
-                      <a href='logout.php' class='btn btn-default btn-flat'>Sign out</a>
-                    </div>";
+                                                <a href='media.php?module=siswa&act=detailprofilsiswa&id=$_SESSION[idsiswa]' class='btn btn-default btn-flat'>Profile</a>
+                                                </div>
+                                                <div class='pull-right'>
+                                                <a href='logout.php' class='btn btn-default btn-flat'>Sign out</a>
+                                                </div>";
                                                 ?>
+                                            </div>
                                         </li>
                                     </ul>
                                 </li>
@@ -202,7 +222,7 @@ if ($_SESSION['login'] == 0) {
                                     </li>
                                 </ul>
                             </li>
-                            <li class="treeview">
+                            <li class="treeview active">
                                 <a href="#">
                                     <i class="fa fa-check"></i>
                                     <span>Absensi</span><i class='fa fa-angle-left pull-right'></i>
@@ -213,14 +233,14 @@ if ($_SESSION['login'] == 0) {
                                             <i class='fa fa-circle-o'></i><span class="title">Mengisi Absensi</span>
                                         </a>
                                     </li>
-                                    <li>
+                                    <li class="active">
                                         <a href="v_data_absen.php">
                                             <i class='fa fa-circle-o'></i><span class="title">Data Absensi</span>
                                         </a>
                                     </li>
                                 </ul>
                             </li>
-                            <li class="active"><a href="tugas.php"><i class="fa fa-book"></i> <span>Tugas</span></a></li>
+                            <li><a href="tugas.php"><i class="fa fa-book"></i> <span>Tugas</span></a></li>
                             <li class="header">Account</li>
                             <li class="treeview">
                                 <a href="#">
@@ -251,85 +271,140 @@ if ($_SESSION['login'] == 0) {
                 <div class="content-wrapper">
                     <!-- Content Header (Page header) -->
                     <section class="content-header">
-
-                        <?php include "../adminku/tabel.php" ?>
-
-                        <!-- Main Footer -->
-                        <footer class="main-footer">
-                            <!-- To the right -->
-                            <div class="pull-right hidden-xs">
-                                Version 1.0
-                            </div>
-                            <!-- Default to the left -->
-                            <strong>Copyright &copy; 2021 <a href="#">inovindo</a>.</strong> All rights reserved.
-                        </footer>
-
-                        <div class="control-sidebar-bg"></div>
+                        <h1>
+                            Data Absensi
+                            <small></small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li><a href="#"><i class="fa fa-calendar"></i><?php include "jam/jam.php" ?></a></li>
+                            <li class="active"><?php include "jam/tanggal.php" ?></li>
+                        </ol>
                     </section>
-                </div><!-- ./wrapper -->
 
+                    <!-- Main content -->
+                    <section class="content">
+                        <div class="table-responsive">
+                            <form method="post" action="./proses.php?kategori=absen_siswa">
+                                <table class="table table-bordered table-striped with-check">
+                                    <thead>
+                                        <tr>
+                                            <th>Hari ke-</th>
+                                            <th>Tanggal</th>
+                                            <th>Kehadiran</th>
+                                            <th>Kegiatan</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $i = 1;
+                                        while ($data_absen = $listDataAbsen->fetch_array()) {
+                                        ?>
+                                            <tr>
+                                                <td scope="row"><?= $i++; ?></td>
+                                                <td><?= $data_absen['waktu']; ?></td>
+                                                <td><?= $data_absen['kehadiran']; ?></td>
+                                                <td><?= $data_absen['kegiatan']; ?></td>
+                                            </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                                <br>
+                                <table class="table table-bordered table-striped with-check">
+                                    <thead>
+                                        <tr>
+                                            <th colspan="3">Jumlah</th>
+                                            <th rowspan="2">Persentase</th>
+                                        </tr>
+                                        <tr>
+                                            <th>H</th>
+                                            <th>I</th>
+                                            <th>S</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </form>
+                        </div>
+                    </section><!-- /.content -->
+                </div><!-- /.content-wrapper -->
 
+                <!-- Main Footer -->
+                <footer class="main-footer">
+                    <!-- To the right -->
+                    <div class="pull-right hidden-xs">
+                        Version 1.0
+                    </div>
+                    <!-- Default to the left -->
+                    <strong>Copyright &copy; <?php echo (int)date('Y'); ?> <a href="https://inovindo.co.id/" target="_blank">INOVINDO DIGITAL MEDIA</a></strong>
+                </footer>
 
+                <!-- Control Sidebar -->
 
+                <!-- Add the sidebar's background. This div must be placed
+           immediately after the control sidebar -->
+                <div class="control-sidebar-bg"></div>
+            </div><!-- ./wrapper -->
 
+            <script src="plugins/jQuery/jquery-1.12.0.min.js"></script>
+            <script src="plugins/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+            <script src="plugins/jquery.ui.touch-punch.min.js"></script>
 
+            <!-- Bootstrap 3.3.2 JS -->
+            <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+            <!-- AdminLTE App -->
+            <script src="dist/js/app.min.js" type="text/javascript"></script>
 
+            <!-- DATATABLES -->
+            <script src="plugins/datatables/media/js/jquery.dataTables.min.js" type="text/javascript"></script>
+            <script src="plugins/datatables/media/js/dataTables.bootstrap.min.js" type="text/javascript"></script>
+            <script src="plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js" type="text/javascript"></script>
+            <script src="plugins/datatables/extensions/Responsive/js/responsive.bootstrap.js" type="text/javascript"></script>
+            <!-- DATATABLES -->
 
-                <script src="plugins/jQuery/jquery-1.12.0.min.js"></script>
-
-
-                <script src="plugins/jquery-ui-1.11.4/jquery-ui.min.js"></script>
-                <script src="plugins/jquery.ui.touch-punch.min.js"></script>
-
-                <!-- Bootstrap 3.3.2 JS -->
-                <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-                <!-- AdminLTE App -->
-                <script src="dist/js/app.min.js" type="text/javascript"></script>
-
-                <!-- DATATABLES -->
-                <script src="plugins/datatables/media/js/jquery.dataTables.min.js" type="text/javascript"></script>
-                <script src="plugins/datatables/media/js/dataTables.bootstrap.min.js" type="text/javascript"></script>
-                <script src="plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js" type="text/javascript"></script>
-                <script src="plugins/datatables/extensions/Responsive/js/responsive.bootstrap.js" type="text/javascript"></script>
-                <!-- DATATABLES -->
-
-                <!--isotope-->
-                <script src="plugins/isotope.pkgd.min.js" type="text/javascript"></script>
-                <script src="plugins/imagesloaded.pkgd.min.js" type="text/javascript"></script>
-                <!--isotope-->
-                <script src="plugins/isotope.pkgd.min.js" type="text/javascript"></script>
-                <script src="plugins/chartJs/Chart.min.js" type="text/javascript"></script>
-                <script src="plugins/chartJs/Chart.Bar.js" type="text/javascript"></script>
-                <script src="dist/js/ando_admin.js" type="text/javascript"></script>
-                <script src="dist/js/mosaicflow.min.js" type="text/javascript"></script>
-                <script src="plugins/file-uploader/js/vendor/jquery.ui.widget.js"></script>
-                <script src="plugins/file-uploader/js/jquery.fileupload.js"></script>
-                <script src="plugins/datepicker/bootstrap-datepicker.min.js"></script>
-                <script src="plugins/ckeditor/ckeditor.js" type="text/javascript"></script>
-                <script src="plugins/ckeditor/adapters/jquery.js" type="text/javascript"></script>
-                <script>
-                    $(function() {
-                        $("#example1").DataTable();
-                        $('#example2').DataTable({
-                            "paging": true,
-                            "lengthChange": false,
-                            "searching": false,
-                            "ordering": true,
-                            "info": true,
-                            "autoWidth": false
-                        });
+            <!--isotope-->
+            <script src="plugins/isotope.pkgd.min.js" type="text/javascript"></script>
+            <script src="plugins/imagesloaded.pkgd.min.js" type="text/javascript"></script>
+            <!--isotope-->
+            <script src="plugins/isotope.pkgd.min.js" type="text/javascript"></script>
+            <script src="plugins/chartJs/Chart.min.js" type="text/javascript"></script>
+            <script src="plugins/chartJs/Chart.Bar.js" type="text/javascript"></script>
+            <script src="dist/js/ando_admin.js" type="text/javascript"></script>
+            <script src="dist/js/mosaicflow.min.js" type="text/javascript"></script>
+            <script src="plugins/file-uploader/js/vendor/jquery.ui.widget.js"></script>
+            <script src="plugins/file-uploader/js/jquery.fileupload.js"></script>
+            <script src="plugins/datepicker/bootstrap-datepicker.min.js"></script>
+            <script src="plugins/ckeditor/ckeditor.js" type="text/javascript"></script>
+            <script src="plugins/ckeditor/adapters/jquery.js" type="text/javascript"></script>
+            <script>
+                $(function() {
+                    $("#example1").DataTable();
+                    $('#example2').DataTable({
+                        "paging": true,
+                        "lengthChange": false,
+                        "searching": false,
+                        "ordering": true,
+                        "info": true,
+                        "autoWidth": false
                     });
-                </script>
-                <script>
-                    $(document).ready(function() {
-                        $('#example3').DataTable({
-                            dom: 'Bfrtip',
-                            buttons: [
-                                'copy', 'csv', 'excel', 'pdf', 'print'
-                            ]
-                        });
+                });
+            </script>
+            <script>
+                $(document).ready(function() {
+                    $('#example3').DataTable({
+                        dom: 'Bfrtip',
+                        buttons: [
+                            'copy', 'csv', 'excel', 'pdf', 'print'
+                        ]
                     });
-                </script>
+                });
+            </script>
         </body>
 
         </html>
